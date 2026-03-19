457a96225c1945ef3f7a0f9b55d156a29c2c237b
Nikita <nikita.petrov.it@gmail.com>
2026-03-20 00:50:43 +0300

    Add SQLi accounts table and related functionality; integrate levels router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth import auth_router
+from app.routers.levels import router as levels_router
 from app.routers.sandbox import router as sandbox_router
 from app.routers.users import user_router
 
@@ -26,6 +27,7 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(sandbox_router)
+app.include_router(levels_router)
 
 
 @app.get("/")
